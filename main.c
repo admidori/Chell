a392ecdf97cb0a14c6033b87915a9e9fa7043092
Aoi Kondo <agotadmidori@gmail.com>
2022-07-05 10:03:02 +0900

    Add: strlen(char *); It is like a strlen() in <string.h>

--- main.c
+++ main.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-void echo_hello(){
-    printf("Hello\n");
-}
-
-int judge(char cmd[]){
-    if(strcmp(cmd,"hello")==0){
-        echo_hello();
+int strlen(char *S){
+    int len=0;
+    while(S[len] != '\0'){
+        len++;
     }
-    else if(strcmp(cmd,"exit")==0){
-        exit(0);
-    }
-    else{
-        return 1;
-    }
-    return 0;
+    return len;
 }
 
 int main(void){
-    int status=0;
     char cmd[256];
     while(1){
-        printf("> ");    scanf("%s",cmd);
-        status=judge(cmd);
-        if(status==1){
-            printf("chell: command not found: %s\n",cmd);
-            status=0;
-        }
+        /* Ready process */
+        printf("#");
+        fgets(&cmd,256,stdin);
+        fflush(stdin);
+
+        /* Search process (brute force) */
+        
     }
-    return 0;
 }
